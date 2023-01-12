32c80893c61a20eb1d8424fe9f3d7a49eec3ae9c
HenrickL <markhenrick.linsangan@gmail.com>
2023-01-12 10:15:12 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,2 @@
+//export const openai_key = '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';
+export const openai_key = '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';
---
+++ pages/chatGPT.js
@@ -0,0 +1,68 @@
+import { useForm } from 'react-hook-form';
+
+import "bootstrap/dist/css/bootstrap.css"
+// import openai from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
+
+import React, { useState } from 'react';
+import { openai_key } from '../config';
+
+export default function ChatGPT() {
+
+    const [responses, setResponses] = useState([]);
+
+    const configuration = new Configuration({
+        apiKey: openai_key,
+    });
+
+
+    const { register, handleSubmit } = useForm();
+
+
+    const onSubmit = async (data) => {
+        try {
+            const openai = new OpenAIApi(configuration);
+            // console.log(openai)
+            const completion = await openai.createCompletion({
+                model: "text-davinci-002",
+                prompt: data.question,
+                max_tokens: 2000,
+                temperature: 0.9,
+            });
+            console.log(completion.data.usage);
+            // setResponse([...response, completion.data.choices[0].text]);
+            setResponses([completion.data.choices[0].text]);
+            // setResponses(['wed']);
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                {
+                    responses.map((responses, index) => (
+                        <div key={index}>
+                            <p>{responses}</p>
+                        </div>
+                    ))
+                }
+
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='form-group'>
+                    <label htmlFor='question'>question</label>
+                    <input
+                        // name='question'
+                        className='form-control'
+                        {...register('question', { required: true })}
+                    />
+                </div>
+                <button type='submit' className='btn btn-primary'>Submit</button>
+            </form>
+        </div>
+    )
+}
